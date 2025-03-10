ba9acd87cb966535eda2539418bc9244365d8c89
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-03-10 19:38:09 +0100

    socket connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use tungstenite::connect;
+use url::Url;
+
+static BINANCE_WS_API: &str = "wss://stream.binance.com:9443";
 fn main() {
-    println!("Hello, world!");
+    let binance_url = format!("{}/ws", BINANCE_WS_API);
+    let (mut socket, response) = connect(Url::parse(&binance_url).unwrap()).expect("Can't connect.");
+
+    println!("Connected to binance stream.");
+    println!("HTTP status code: {}", response.status());
+    println!("Response headers:");
+    for (ref header, header_value) in response.headers() {
+        println!("- {}: {:?}", header, header_value);
+    }
 }
